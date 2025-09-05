1b907de09c2b0b9c9eac5c9c644de1fc766e4190
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-09-05 15:29:38 +0600

    feat: restructure Home component to include various sections for improved layout

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,10 +1,29 @@
-import { ModeToggle } from "@/components/ui/ModeToggle";
+import { CourseCategories } from "@/components/home/course-categories";
+import { FAQSection } from "@/components/home/faq-section";
+import { FeaturedCourses } from "@/components/home/featured-courses";
+import { HeroSection } from "@/components/home/hero-section";
+import { InstructorsSection } from "@/components/home/instructors-section";
+import { NewsletterSection } from "@/components/home/newsletter-section";
+import { PricingSection } from "@/components/home/pricing-section";
+import { StatsSection } from "@/components/home/stats-section";
+import { TestimonialsSection } from "@/components/home/testimonials-section";
+import { WhyChooseUs } from "@/components/home/why-choose-us";
 
 export default function Home() {
   return (
     <div>
-      <h1 className="text-lg">This is page component</h1>
-      <ModeToggle />
+      <main>
+        <HeroSection />
+        <FeaturedCourses />
+        <WhyChooseUs />
+        <CourseCategories />
+        <StatsSection />
+        <TestimonialsSection />
+        <InstructorsSection />
+        <PricingSection />
+        <FAQSection />
+        <NewsletterSection />
+      </main>
     </div>
   );
 }
