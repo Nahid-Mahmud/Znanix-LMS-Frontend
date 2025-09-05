bada6bd6694bc03e3fc36360077a3e9e5193019e
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-09-05 15:29:21 +0600

    feat: add various asset images and create index for asset exports

---
+++ src/assets/index.ts
@@ -0,0 +1,66 @@
+// Import all assets and export them
+import dataSciencePythonAnalytics from "./data-science-python-analytics.jpg";
+import dataSciencePythonProgrammingChartsGraphsAnal from "./data-science-python-programming-charts-graphs-anal.jpg";
+import digitalMarketingCourse from "./digital-marketing-course.png";
+import digitalMarketingSocialMediaAdvertisingAnalyti from "./digital-marketing-social-media-advertising-analyti.jpg";
+import instructorAlex from "./instructor-alex.jpg";
+import instructorEmma from "./instructor-emma.jpg";
+import instructorJessica from "./instructor-jessica.jpg";
+import instructorMichael from "./instructor-michael.jpg";
+import instructorRobert from "./instructor-robert.jpg";
+import instructorSarah from "./instructor-sarah.jpg";
+import machineLearningCourse from "./machine-learning-course.png";
+import placeholderLogoPng from "./placeholder-logo.png";
+import placeholderLogoSvg from "./placeholder-logo.svg";
+import placeholderUser from "./placeholder-user.jpg";
+import placeholderJpg from "./placeholder.jpg";
+import placeholderSvg from "./placeholder.svg";
+import professionalCeoHeadshot from "./professional-ceo-headshot.png";
+import professionalCpoHeadshot from "./professional-cpo-headshot.jpg";
+import professionalCtoHeadshot from "./professional-cto-headshot.jpg";
+import professionalHeadshotManDataScientistMicrosoft from "./professional-headshot-man-data-scientist-microsoft.jpg";
+import professionalHeadshotManMarketingManager from "./professional-headshot-man-marketing-manager.jpg";
+import professionalHeadshotManUxDesignerApple from "./professional-headshot-man-ux-designer-apple.jpg";
+import professionalHeadshotWomanDataAnalyst from "./professional-headshot-woman-data-analyst.jpg";
+import professionalHeadshotWomanMarketingDirectorMet from "./professional-headshot-woman-marketing-director-met.jpg";
+import professionalHeadshotWomanSoftwareEngineerGoog from "./professional-headshot-woman-software-engineer-goog.jpg";
+import professionalHeadshotYoungManSoftwareDeveloper from "./professional-headshot-young-man-software-developer.jpg";
+import reactNextjsProgramming from "./react-nextjs-programming.jpg";
+import studentsLearningOnlineWithLaptopsAndBooksIn from "./students-learning-online-with-laptops-and-books-in.jpg";
+import uiUxDesignCourse from "./ui-ux-design-course.png";
+import webDevelopmentCodingScreenWithHtmlCssJavasc from "./web-development-coding-screen-with-html-css-javasc.jpg";
+import webDevelopmentCodingPng from "./web-development-coding.png";
+
+export {
+  dataSciencePythonAnalytics,
+  dataSciencePythonProgrammingChartsGraphsAnal,
+  digitalMarketingCourse,
+  digitalMarketingSocialMediaAdvertisingAnalyti,
+  instructorAlex,
+  instructorEmma,
+  instructorJessica,
+  instructorMichael,
+  instructorRobert,
+  instructorSarah,
+  machineLearningCourse,
+  placeholderLogoPng,
+  placeholderLogoSvg,
+  placeholderUser,
+  placeholderJpg,
+  placeholderSvg,
+  professionalCeoHeadshot,
+  professionalCpoHeadshot,
+  professionalCtoHeadshot,
+  professionalHeadshotManDataScientistMicrosoft,
+  professionalHeadshotManMarketingManager,
+  professionalHeadshotManUxDesignerApple,
+  professionalHeadshotWomanDataAnalyst,
+  professionalHeadshotWomanMarketingDirectorMet,
+  professionalHeadshotWomanSoftwareEngineerGoog,
+  professionalHeadshotYoungManSoftwareDeveloper,
+  reactNextjsProgramming,
+  studentsLearningOnlineWithLaptopsAndBooksIn,
+  uiUxDesignCourse,
+  webDevelopmentCodingScreenWithHtmlCssJavasc,
+  webDevelopmentCodingPng,
+};
