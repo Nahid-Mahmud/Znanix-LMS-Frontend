7c8ee84096a0ce208c54782b36e19259fdfc354c
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-09-05 15:31:00 +0600

    fix: add border to header and adjust padding in DashboardWrapper

--- src/wrapper/DashboardWrapper.tsx
+++ src/wrapper/DashboardWrapper.tsx
@@ -11,7 +11,7 @@ export default function DashboardWrapper({ children }: { children: React.ReactNo
       <SidebarProvider>
         <AppSidebar />
         <SidebarInset>
-          <header className="flex h-14 shrink-0 items-center gap-2">
+          <header className="flex h-14 shrink-0 items-center gap-2 border-b ">
             <div className="flex flex-1 items-center gap-2 px-3">
               <SidebarTrigger />
               {/* <Separator orientation="vertical" className="mr-2 data-[orientation=vertical]:h-4" /> */}
@@ -28,7 +28,7 @@ export default function DashboardWrapper({ children }: { children: React.ReactNo
               <ModeToggle />
             </div>
           </header>
-          <div className="flex flex-1 flex-col gap-4 px-4 py-10">{children}</div>
+          <div className="flex flex-1 flex-col gap-4 px-4 py-5">{children}</div>
         </SidebarInset>
       </SidebarProvider>
     </div>
