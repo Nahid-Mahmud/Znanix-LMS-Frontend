6026cc1d44f2b60904f43d18761fd9711eaca1bc
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-09-05 15:29:42 +0600

    refactor: simplify Dashboard component to return a basic greeting

--- src/app/(dashboard)/admin-dashboard/page.tsx
+++ src/app/(dashboard)/admin-dashboard/page.tsx
@@ -1,7 +1,3 @@
 export default function Dashboard() {
-  return (
-    <div>
-      <h1 className="text-lg min-h-[500000px]">This is page component</h1>
-    </div>
-  );
+  return <div>hello</div>;
 }
