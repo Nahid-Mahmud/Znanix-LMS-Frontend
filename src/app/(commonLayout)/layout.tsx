f67f4640ad7a5f73dfeeeea4b8b0c2f5abe5a19f
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-09-05 15:29:34 +0600

    feat: create CommonLayout component with header and footer structure

---
+++ src/app/(commonLayout)/layout.tsx
@@ -0,0 +1,12 @@
+import { Footer } from "@/components/common/footer";
+import { Header } from "@/components/common/header";
+
+export default function CommonLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="min-h-screen grid grid-rows-[auto_1fr_auto]">
+      {/* <Header /> */}
+      {children}
+      <Footer />
+    </div>
+  );
+}
